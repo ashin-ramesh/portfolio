f56df811ec050850549f781144d09d3ac472928b
ashin-ramesh <ashinkdb17@gmail.com>
2026-03-24 22:50:27 +0530

    initial commit

---
+++ frontend/script.js
@@ -0,0 +1,37 @@
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  // Get values from the form
+  const name = form.elements[0].value;
+  const email = form.elements[1].value;
+  const message = form.elements[2].value;
+
+  const data = { name, email, message };
+
+  // Send data to the backend running on port 5000
+  fetch("http://localhost:5000/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+  .then(async function (response) {
+    if (!response.ok) {
+      const err = await response.text();
+      throw new Error(err);
+    }
+    return response.json();
+  })
+  .then(function (result) {
+    alert("Message sent succesfully  ");
+    form.reset();
+  })
+  .catch(function (err) {
+    console.error(err);
+    alert("Error occurred ");
+  });
+});
